8626a82335abd2f78ac146f6ab1738501c82a9e1
Sfa11eN <sfallenn@gmail.com>
2023-11-15 21:19:18 +0300

    completed

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,53 @@ public class Main {
         var paper = 763789;
         System.out.println(dog + " " + cat + " " + paper);
 
+        System.out.println(" ");
+
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
         System.out.println(dog + " " + cat + " " + paper);
+
+        System.out.println(" ");
 
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
         System.out.println(dog + " " + cat + " " + paper);
 
-        var friend = 19;
-        System.out.println( friend );
-        friend=friend+2;
-        System.out.println( friend );
-        friend=friend/7;
-        System.out.println( friend );
+        System.out.println(" ");
 
-        var frog=3.5;
-        System.out.println( frog );
-        frog=frog*10;
-        System.out.println( frog );
-        frog=frog/3.5;
-        System.out.println( frog );
-        frog=frog+4;
-        System.out.println( frog );
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println(" ");
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        System.out.println(" ");
+
+        var FighterWeight = 78.2;
+        var fighterWeight = 82.7;
+        var result = FighterWeight + fighterWeight;
+        System.out.println(" Вместе бойцы весят " + result + " кг ");
+        var result2 = FighterWeight - fighterWeight;
+        System.out.println(" Разница между бойцами " + result2 + " кг ");
+        var result3 = fighterWeight - FighterWeight;
+        System.out.println(" Вычитание из большей массы меньшей " + result3 + " кг ");
+        var result4 = (fighterWeight % FighterWeight);
+        System.out.println(" С функцией остаток от деления ПО ПОДСКАЗКЕ В УСЛОВИИ УРОКА " + result4 + " кг " + " НО ЭТО ЯВНО НЕПРАВИЛЬНО");
+
+
     }
 }
