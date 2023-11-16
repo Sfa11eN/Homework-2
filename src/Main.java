5266b9de98a3a8116b2f2c5e364676c913295684
Sfa11eN <sfallenn@gmail.com>
2023-11-16 22:13:36 +0300

    completed with questions

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,27 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" Задача 1 ");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog + " " + cat + " " + paper);
 
-        System.out.println(" ");
+        System.out.println(" Задача 2 ");
 
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
         System.out.println(dog + " " + cat + " " + paper);
 
-        System.out.println(" ");
+        System.out.println(" Задача 3 ");
 
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
         System.out.println(dog + " " + cat + " " + paper);
 
-        System.out.println(" ");
+        System.out.println(" Задача 4 ");
 
         var friend = 19;
         System.out.println(friend);
@@ -30,7 +31,7 @@ public class Main {
         friend = friend / 7;
         System.out.println(friend);
 
-        System.out.println(" ");
+        System.out.println(" Задача 5 ");
 
         var frog = 3.5;
         System.out.println(frog);
@@ -41,32 +42,33 @@ public class Main {
         frog = frog + 4;
         System.out.println(frog);
 
-        System.out.println(" ");
+        System.out.println(" Задача 6 ");
 
-        var FighterWeight = 78.2;
-        var fighterWeight = 82.7;
-        var result = FighterWeight + fighterWeight;
+        var fighterWeight1 = 78.2;
+        var fighterWeight2 = 82.7;
+        var result = fighterWeight1 + fighterWeight2;
         System.out.println(" Вместе бойцы весят " + result + " кг ");
-        var result2 = FighterWeight - fighterWeight;
+        var result2 = fighterWeight1 - fighterWeight2;
         System.out.println(" Разница между бойцами " + result2 + " кг ");
-        var result3 = fighterWeight - FighterWeight;
+
+        System.out.println(" Задача 7 ");
+
+        var result3 = fighterWeight2 - fighterWeight1;
         System.out.println(" Вычитание из большей массы меньшей " + result3 + " кг ");
-        var result4 = fighterWeight % FighterWeight;
-        System.out.println(" С функцией остаток от деления ПО ПОДСКАЗКЕ В УСЛОВИИ УРОКА " + result4 + " кг " + " НО ЭТО ЯВНО НЕПРАВИЛЬНО");
+        var result4 = fighterWeight2 % fighterWeight1;
+        System.out.println(" С функцией остаток от деления  " + result4 + " кг " + " ");
 
-        System.out.println(" ");
+        System.out.println(" Задача 8 ");
 
-        var Time=640;
-        var WorkTime=8;
-        var Workers=Time/WorkTime;
-        System.out.println("Всего работников в компании " + Workers + " человек " );
-        var Reinforcement=94;
+        var Time = 640;
+        var WorkTime = 8;
+        var Workers = Time / WorkTime;
+        System.out.println("Всего работников в компании " + Workers + " человек ");
+        var Reinforcement = 94;
         System.out.println("Допустим что в компании работает на " + Reinforcement + " сотрудника больше ");
-        var Pull=Workers+Reinforcement;
-        var FinalTime=Pull*8;
+        var Pull = Workers + Reinforcement;
+        var FinalTime = Pull * 8;
         System.out.println("Если в компании работает " + Pull + " человека ," + " то всего " + FinalTime + " часов работы может быть поделено между сотрудниками ");
-
-
 
 
     }
